335fdb7425ca0d229b8f7dc27136abb776c74392
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-07-16 20:49:13 +0000

    3. Retângulo

---
+++ Heranca/retangulo/Retangulo.java
@@ -0,0 +1,20 @@
+public class Retangulo extends FormaGeometrica {
+
+    private double largura;
+    private double altura;
+    
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+}
