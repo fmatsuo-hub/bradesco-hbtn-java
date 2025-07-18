a2c167aafc798f692c1ddda6c6e3db82c4b43467
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-07-18 20:33:10 +0000

    0. Pedido Livraria

---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,21 @@
+package Heranca_Exercicios.pedido_livraria;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        double total = 0.0;
+        for (ItemPedido item : itens) {
+            double precoLiquido = item.getProduto().obterPrecoLiquido();
+            total += precoLiquido * item.getQuantidade();
+        }
+        total -= total * (percentualDesconto / 100);
+        return total;
+    }
+}
