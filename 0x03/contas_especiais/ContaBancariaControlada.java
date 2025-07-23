b4e49b7c0da402ed05e119f0367bc22ab9c8f4f1
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-07-23 19:58:52 +0000

    2. Conta Bancária Controlada/Tarifada

---
+++ 0x03/contas_especiais/ContaBancariaControlada.java
@@ -0,0 +1,23 @@
+public class ContaBancariaControlada extends ContaBancariaBasica {
+    
+    private double saldoMinimo;
+    private double valorPenalidade;
+
+    public ContaBancariaControlada(String numeracao, double taxaJurosAnual, double saldoMinimo, double valorPenalidade) {
+        super(numeracao, taxaJurosAnual);
+        this.saldoMinimo = saldoMinimo;
+        this.valorPenalidade = valorPenalidade;
+    }
+
+    @Override
+    public void aplicarAtualizacaoMensal() {
+        super.aplicarAtualizacaoMensal();
+        if (getSaldo() < saldoMinimo) {
+            try {
+                super.sacar(valorPenalidade);
+            } catch (Exception e) {
+                // saldo negativo ou insuficiente
+            }
+        }
+    }
+}
