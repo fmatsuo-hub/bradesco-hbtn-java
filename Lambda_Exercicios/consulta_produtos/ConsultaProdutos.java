c32c08ea05ed669021815da9527d71f75e5f6d76
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-08-13 20:37:12 +0000

    0. Consultar Produtos

---
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterio) {
+        List<Produto> filtrados = new ArrayList<>();
+        for (Produto p : produtos) {
+            if (criterio.testar(p)) {
+                filtrados.add(p);
+            }
+        }
+        return filtrados;
+    }
+}
