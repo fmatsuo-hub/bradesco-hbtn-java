18db60060c464a92214bde335e7e4de9a005e3fe
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-08-20 20:41:16 +0000

    1. Idades das pessoas agrupadas por cargo

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.mapping(Pessoa::getIdade, Collectors.toList())
+                ));
+    }
+}
