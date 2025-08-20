ad89a9d7b99e1d09f976ad62c74783b43db36c7c
Fabio Matsuo <fabio.matsuo@bradesco.com.br>
2025-08-20 20:33:37 +0000

    Ajuste saída

--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Pessoa {
     private int codigo;
     private String nome;
@@ -15,7 +17,8 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s %d R$ %.6f",
+        return String.format(new Locale("pt", "BR"),
+                "[%d] %s %s %d R$ %.6f",
                 codigo, nome, cargo, idade, salario);
     }
 
