49404405b8553b165ab6fdda1f11c2bf8c335c3f
Fabio <fabiolmatsuo@gmail.com>
2025-10-18 11:59:09 -0300

    Java - Generics

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca<T extends Midia> {
+    private List<T> listaMidias = new ArrayList<>();
+
+    public void adicionarMidia(T midia) {
+        listaMidias.add(midia);
+    }
+
+    public List<T> obterListaMidias() {
+        return listaMidias;
+    }
+}
